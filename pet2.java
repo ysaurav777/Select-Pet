d0fd2885ec3906d93794aa41222fbe36aeb69182
Saurav Yadav <ysaurav777@gmail.com>
2023-05-21 11:15:37 +0530

    first commit

---
+++ pet2.java
@@ -0,0 +1,15 @@
+let petsImageUrls = {
+    dog: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-dog-img.png",
+    cat: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-cat-img.png",
+    parrot: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-parrot-img.png",
+    spider: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-spider-img.png",
+    rabbit: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-rabbit-img.png"
+};
+
+let petImgEl = document.getElementById("petImg");
+let petSelectEl = document.getElementById("petSelect");
+
+petSelectEl.addEventListener("change", function(event) {
+    let selectEl = event.target.value;
+    petImgEl.src = petsImageUrls[selectEl];
+});
